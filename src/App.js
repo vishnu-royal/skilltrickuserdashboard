90db917a774660e518465a2ff4e228ebe9badd3e
vishnu-royal <vishnuroyal1992@gmail.com>
2025-03-27 19:07:35 +0530

    Add file description

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Search from './Pages/Search';
 import Index from './Pages/Index';
-// import NewChat from "./Pages/NewChat";
+import NewChat from "./Pages/NewChat";
 import Chat from "./Pages/Chat";
 import Favorites from "./Pages/Favorites";
 import Settings from "./Pages/Settings";
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/Search" element={<Search />} />
-        {/* <Route path="/new-chat" element={<NewChat />} /> */}
+        <Route path="/new-chat" element={<NewChat />} />
         <Route path="/Chat" element={<Chat />} />
         <Route path="/Favorites" element={<Favorites />} />
         <Route path="/Settings" element={<Settings />} />
